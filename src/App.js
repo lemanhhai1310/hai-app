adce3e66e95f98e2a9b2d4f2093652bc80b13bae
macbook <hai.lemanh.1310@gmail.com>
2019-04-03 03:32:18 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,48 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function One() {
+	return <div>Cach so 1</div>;
+}
+
+var Two = function () {
+	return(
+		<div>
+			Cách số 2
+		</div>
+	);
+}
+
+var Three = () => (
+	<div>
+		Cách số 3
+	</div>
+);
+
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo"/>
+					<One/>
+					<Two/>
+					<Three/>
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
