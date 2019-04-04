8c21e834e2a291e44ba105be75f5b252956e7971
macbook <hai.lemanh.1310@gmail.com>
2019-04-04 17:51:27 +0700

    update nội dung

--- src/Component/Noidung.js
+++ src/Component/Noidung.js
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
 
 class Noidung extends Component {
+
+	componentWillReceiveProps(nextProps, nextContext) {
+		console.log('componentWillReceiveProps của Noidung.js');
+	}
+
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+		console.log('shouldComponentUpdate của Noidung.js');
+		return true;
+	}
+
+	componentWillUpdate(nextProps, nextState, nextContext) {
+		console.log('componentWillUpdate của Noidung.js');
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		console.log('componentDidUpdate của Noidung.js');
+	}
+
 	render() {
 		return (
 			<div>
